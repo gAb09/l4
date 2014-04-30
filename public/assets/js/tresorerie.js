f4ddef537c74cf543245065912484e7e0c4b34b1
gAb09 <brunogabiot@club-internet.fr>
2014-04-30 19:57:51 +0200

    depot

---
+++ public/assets/js/tresorerie.js
@@ -0,0 +1,127 @@
+/*----------   Bascule du signe du montant (pages Recettes/Dépenses & Écritures) -----------*/
+function initialise() {
+	bascule_signe();
+	banque();
+}
+
+function initialiseTypes() {
+	justifRequis();
+}
+
+function separateur(select1) {
+	var span = document.getElementById("sep1");
+	sep = separateurs[select1.value];
+	span.innerHTML = sep;
+}
+
+
+function separateur2(select2) {
+	var span = document.getElementById("sep2");
+	sep = separateurs[select2.value];
+	span.innerHTML = sep;
+	// alert(span);
+}
+
+function justifRequis() {
+	var div = document.getElementById("req_justif");
+	var label = document.getElementById("req_justif_label");
+	var checkbox = document.getElementById("req_justif_checkbox");
+
+	if (checkbox.checked == 1)
+	{
+		label.innerHTML="Le champ \"Justificatif\" est requis";
+			div.className=""; /* Si oui on affiche la div séparateur */
+	}
+	else
+	{
+		label.innerHTML="Le champ \"Justificatif\" n’est pas requis";
+			div.className="invisible"; /* Si oui on affiche la div séparateur */
+	}
+}
+
+
+
+function bascule_signe() {
+	var label = document.getElementById("banque2_label");
+
+	if (document.form.signe_1.checked == 1)
+	{
+		document.getElementById("montant").style.color="red"; /* aFA passer par les classe de span */
+		label.innerHTML="Vers";
+	}
+	else
+	{
+		document.getElementById("montant").style.color="blue";
+		label.innerHTML="Depuis";
+	}
+}
+
+
+
+/*----------   Affichage de la banque de destination (pages Écritures)-----------*/
+function banque() {
+	var div = document.getElementById("banque2");
+	var form = document.form;
+	var select = document.getElementById("double");
+	var label = document.getElementById("label_flag");
+
+	// alert(separateurs);
+
+	/* Si le "type_id" sélectionné via le formulaire est dans la liste des types qui requièrent une banque liée
+	(c'est le tableau $type_dble_ecriture passé en json depuis ecriture_form.blade.php */
+		if (select.checked === true)
+		{
+			div.className="input"; /* Si oui on affiche la div banque liée */
+			label.innerHTML="Écriture double";
+		}
+		else
+		{
+			div.className="input invisible";
+			label.innerHTML="Écriture simple";
+		}
+
+	}
+
+	/*----------   Affichage des formulaires d'édtion des notes (widget Notes) -----------*/
+	function show_form_creer() {
+		document.getElementById("coulisse_form_creer").className="visible";
+	}
+
+	function show_form_editer() {
+		document.getElementById("coulisse_form_editer").className="visible";
+	}
+
+
+/*----------  Pointage web = marquage comme pointé selon l'état fourni sur le site Caisse d´Épargne 
+Changement de la couleur du fond de ligne et update du pointage (pages "Pointage") -----------*/
+function bascule_pointage(xxx) {
+		// Obtenir le <tr> parent de l'input select name"pointage" et changer sa classe 
+		var row = xxx.parentNode.parentNode.parentNode;
+		var input = row.childNodes[1].childNodes[1].childNodes[2];
+
+		// alert(input.value); /* CTRL */
+
+
+		if (row.className == "surlignage st_prev") {
+			row.className = "surlignage st_emise";
+			return;
+		}
+
+		if (row.className == "surlignage st_emise") {
+			row.className = "surlignage st_www";
+			return;
+		}
+
+
+		if (row.className == "surlignage st_www") {
+			row.className = "surlignage st_releve";
+			return;
+		}
+
+
+		if (row.className == "surlignage st_releve") {
+			row.className = "surlignage st_prev";
+		}
+
+	}
+
