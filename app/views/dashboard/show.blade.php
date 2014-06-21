1b0f155c7f5f613db58f841d8640be5228228ed9
gAb09 <brunogabiot@club-internet.fr>
2014-06-21 16:43:18 +0200

    mise à jour des vues identification

---
+++ app/views/dashboard/show.blade.php
@@ -0,0 +1,14 @@
+@extends('compta/layout')
+
+@section('contenu')
+<div class="" id="show">
+	<h5 style="display:inline">Login :</h5>
+	{{ Auth::user()->login }}
+	<br />
+	<h5 style="display:inline">Mail :</h5>
+	{{ Auth::user()->mail }}
+</div>
+@stop
+
+@section('script')
+@stop
