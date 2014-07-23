e9a2652d2d81d1a6a00658ea9d7f832e641f2a87
gAb09 <brunogabiot@club-internet.fr>
2014-07-24 00:26:19 +0200

    correction calcul solde à chaque ligne 2

--- app/views/compta/pointage/main.blade.php
+++ app/views/compta/pointage/main.blade.php
@@ -66,12 +66,11 @@
 
 
 	<tbody class="replie" id="corps{{$ecriture->mois_valeur}}">
-		DIFF
 		<?php $prev_mois = $ecriture->mois_valeur ?>
 	<?php $solde = $solde + ($ecriture->montant*$ecriture->signe->signe); ?>
 		@include('compta/pointage/row')
 		@else
-IDEM
+
 	<?php $solde = $solde + ($ecriture->montant*$ecriture->signe->signe); ?>
 		@include('compta/pointage/row')
 		@endif
