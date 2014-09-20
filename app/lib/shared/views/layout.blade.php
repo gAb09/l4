f4e55888c7ac9170c2a4f263d6a71ffe115c88c8
gAb09 <brunogabiot@club-internet.fr>
2014-09-20 20:08:36 +0200

    deplacement fonctionnalité fillFormFor Create

---
+++ app/lib/shared/views/layout.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>
+		@section('titre')
+		{{ isset($titre_page) ? $titre_page : Menu::where('nom_sys', Request::segment(1))->get()[0]->etiquette }}
+		@show
+
+	</title>
+	<link rel="shortcut icon" href="/assets/img/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link href="/assets/css/tresorerie.css" rel="stylesheet" type="text/css">
+	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
+	<script src="/assets/js/tresorerie.js"></script>
+
+</head>
+
+
+
+
+
+<body @section('body')>
+	@show
+
+	<div class="container-fluid">
+
+
+
+		<!-- - - - - - - - - - - - - - - - Messages - - - - - - - - - - - - - - -->
+		<div class="span12 messages">
+
+			@include('shared/views/messages')
+		</div>
+
+
+
+		<header class="row-fluid">
+
+
+			<!-- - - - - - - - - - - - - - - - MENU SECTIONS - - - - - - - - - - - - - - -->
+
+			<nav class="navbar sections span12">
+				@include('shared/views/menuSections')
+			</nav>
+
+
+			<!-- - - - - - - - - - - - - - SOUS MENU - - - - - - - - - - - - - - -->
+
+			<nav class="navbar menus span9">
+				@include('shared/views/menus')
+			</nav>
+
+
+			<!-- - - - - - - - - - - - - - - - USER / DECONNEXION - - - - - - - - - - - - - - -->
+
+			<div class="span3 user_widget">
+				@include('shared/views/user_widget')
+			</div>
+
+		</header>
+
+		<!-- - - - - - - - - - - - - - - - TOP CONTENT (2 zones) - - - - - - - - - - - - - - -->
+
+
+		<div class="row-fluid" style="padding-bottom:5px">
+
+			<div class="span6">
+				@yield('topcontent1')
+			</div>
+
+			<div class="span6">
+				@yield('topcontent2')
+			</div> 
+		</div>
+
+		<!-- - - - - - - - - - - - - - - - CONTENU - - - - - - - - - - - - - - -->
+
+		<div class="row-fluid">
+			<div>
+				@yield('contenu')
+			</div>
+		</div>
+
+
+		<!-- - - - - - - - - - - - - - - - FOOTER - - - - - - - - - - - - - - -->
+
+		<footer>
+			<hr>
+			@section('tresorerie/footer')
+			© gAb
+			@show
+		</footer>
+
+		<!-- - - - - - - - - - - - - - - - BARRE COMMANDES - - - - - - - - - - - - - - -->
+
+		<div class="zapette">
+
+			@yield('zapette')
+			<br />ZAPETTE
+
+		</div>
+
+
+	</div>
+	@section('script')
+
+	@show
+</body>
+</html>
