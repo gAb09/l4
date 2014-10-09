b76af4be478cef255dcd8499c8d14f7274f53a45
gAb09 <brunogabiot@club-internet.fr>
2014-10-09 20:53:11 +0200

    affichages double des dates via infobulles

--- app/lib/frontend/tresorerie/views/pointage/row.blade.php
+++ app/lib/frontend/tresorerie/views/pointage/row.blade.php
@@ -1,58 +1,70 @@
 <tr 
 id="{{ $ecriture->id }}" 
 class="surlignage {{$ecriture->statut->classe}}" 
-	ondblclick = document.location.href="{{ URL::action('EcritureController@edit', [$ecriture->id]) }}">
+ondblclick = document.location.href="{{ URL::action('EcritureController@edit', [$ecriture->id]) }}">
 
-<td>
-	{{-- $Statuts ok est le tableau des statuts accessibles depuis cette page --}}
-	@if (strpos($statuts_accessibles, (string)$ecriture->statut->rang) !== false)
-	{{ Form::open(array('name' => 'pointage', 'action' => ['PointageController@pointage', $ecriture->id, $statuts_accessibles], 'method' => 'post', 'class' => 'pointage')) }}
+	<!-- Statut -->
+	<td>
+		{{-- $Statuts ok est le tableau des statuts accessibles depuis cette page --}}
+		@if (strpos($statuts_accessibles, (string)$ecriture->statut->rang) !== false)
+		{{ Form::open(array('name' => 'pointage', 'action' => ['PointageController@pointage', $ecriture->id, $statuts_accessibles], 'method' => 'post', 'class' => 'pointage')) }}
 
-	{{ Form::hidden('rang', $ecriture->statut->rang, array('id' => 'input', 'class' => '')) }}
+		{{ Form::hidden('rang', $ecriture->statut->rang, array('id' => 'input', 'class' => '')) }}
 
-	{{ Form::button('', array('class' => 'btn btn-link iconemedium toggle', 'style' => '', 'OnClick' => 'bascule_statut_pointage(this);submit();' )) }}
+		{{ Form::button('', array('class' => 'btn btn-link iconemedium toggle', 'style' => '', 'OnClick' => 'bascule_statut_pointage(this);submit();' )) }}
 
-	{{ form::close() }}
-	@endif
-</td>
+		{{ form::close() }}
+		@endif
+	</td>
 
-<td title="Émission : {{ Date::courteSec($ecriture->date_emission) }}">
-	{{ $ecriture->mois  }}
-	{{ Date::courte($ecriture->date_valeur) }}
-</td>
+	<!-- Dates -->
+	<td id="valeur{{ $ecriture->id }}" class="info">
+		{{ Date::courte($ecriture->date_valeur) }}
+		<span>
+			Date d’émission : {{ Date::courte($ecriture->date_emission) }}
+		</span>
 
-<td>
-	{{ $ecriture->libelle }}
-	@if($ecriture->libelle_detail)
-	— 
-	{{ $ecriture->libelle_detail }}
-	@endif
-</td>
+	</td>
 
 
-<td class="{{$ecriture->signe->nom_sys}}">
-
-	@if($ecriture->signe_id == 1)
-	{{ Nbre::francais_insec($ecriture->montant) }}
-	@endif
-</td>
-
-<td class="{{$ecriture->signe->nom_sys}}">
-
-	@if($ecriture->signe_id == 2)
-	{{ Nbre::francais_insec($ecriture->montant) }}
-	@endif
-</td>
+	<!-- Libellé -->
+	<td>
+		{{ $ecriture->libelle }}
+		@if($ecriture->libelle_detail)
+		— 
+		{{ $ecriture->libelle_detail }}
+		@endif
+	</td>
 
 
-@if($solde >= 0)
-<td class="recette">
-	@else
-	<td class="depense">
+	<!-- Montant -->
+	<td class="{{$ecriture->signe->nom_sys}}">
+
+		@if($ecriture->signe_id == 1)
+		{{ Nbre::francais_insec($ecriture->montant) }}
+		@endif
+	</td>
+
+	<td class="{{$ecriture->signe->nom_sys}}">
+
+		@if($ecriture->signe_id == 2)
+		{{ Nbre::francais_insec($ecriture->montant) }}
+		@endif
+	</td>
+
+
+	<!-- Solde -->
+	<td 
+		@if($solde >= 0)
+		class="recette">
+		@else
+		class="depense">
 		@endif
 		{{ Nbre::francais_insec($solde) }}
 	</td>
 
+
+	<!-- Type -->
 	<td>
 		{{ $ecriture->type->nom }}
 		@if($ecriture->justificatif)
@@ -61,6 +73,8 @@ class="surlignage {{$ecriture->statut->classe}}"
 		{{ $ecriture->justificatif }}
 	</td>
 
+
+	<!-- Banque -->
 	<td>{{ $ecriture->banque->nom }}
 		@if($ecriture->double_flag)
 		@if($ecriture->signe->signe == -1)
@@ -72,12 +86,11 @@ class="surlignage {{$ecriture->statut->classe}}"
 		@endif
 	</td>
 
-	<td>
-		{{ Date::courte($ecriture->date_emission) }}
-	</td>
 
+	<!-- Edit -->
 	<td>
-		<a class="iconemedium edit" href ="{{ URL::action('EcritureController@edit', [$ecriture->id]) }}"></a>
+		<a class="iconemedium edit" href ="{{ URL::action('EcritureController@edit', [$ecriture->id]) }}">
+		</a>
 	</td>
 
 </tr>
