43d281adc8bedcf7369878b018d42d61b7a750f9
gAb09 <brunogabiot@club-internet.fr>
2014-10-17 19:42:30 +0200

    modification convention ecriture fichiers des validations donc reprises dans les controleurs 2

---
+++ app/lib/backend/utilisateurs/UtilisateurValidation.php
@@ -0,0 +1,21 @@
+<?php namespace Lib\Validations;
+
+class UtilisateurValidation extends ValidationBase
+{
+
+
+	protected $rules = array(
+		'login' => 'required|unique:utilisateurs,login',
+		'mdp' => 'required',
+		'mail' => 'required|unique:utilisateurs,mail',
+		);
+
+	public $messages = array(
+		'login.unique' => 'Ce Login existe déjà',
+		'login.required' => 'Saisissez un Login',
+		'mdp.required' => 'Saisissez un Mot de Passe',
+		'mail.unique' => 'Ce mail existe déjà',
+		'mail.required' => 'Saisissez une adresse mail',
+		);
+
+}
