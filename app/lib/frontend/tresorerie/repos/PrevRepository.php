d82eb58a20b52d1d500f8ff78180d8da23f627d7
gAb09 <brunogabiot@club-internet.fr>
2014-10-21 23:13:26 +0200

    Prévisionnel : correctif traitement ecriture double non principale. Prise en compte des ordres de priorités des banques.

--- app/lib/frontend/tresorerie/repos/PrevRepository.php
+++ app/lib/frontend/tresorerie/repos/PrevRepository.php
@@ -65,7 +65,7 @@ class PrevRepository {
 		/* ----- Traitement des soldes par banques ----- */
 		$ecritures->each(function($ecriture) use ($ecritures, $order, $banques) {
 
-			/* On récupère la liste des banques à afficher */
+			/* On intègre signe et montant, et réassigne $ecriture->montant */
 			$ecriture->montant = $ecriture->montant * $ecriture->signe->signe;
 
 			foreach ($banques as $bank) {
@@ -73,6 +73,8 @@ class PrevRepository {
 				/* On calcule les soldes de chaque banque à chaque ligne
 				Attention le calcul est différent s'il s'agit d'une écriture double ou simple */
 
+				/* On conserve las soldes de l'écriture précédente 
+				pour déterminer s'ils seront affichés ou non. */
 				$prev_solde_1 = $this->solde[1];
 				$prev_solde_2 = $this->solde[2];
 				$prev_solde_3 = $this->solde[3];
@@ -91,25 +93,39 @@ class PrevRepository {
 					if (!is_null($ecriture->is_double) and $ecriture->banque_id == 1) {
 						$this->solde[1] +=  $ecriture->montant;
 						$this->solde[$ecriture->banque_soeur_id] -=  $ecriture->montant;
-						// $this->solde['total'] += $ecriture->montant;
 					}
 
-					/* Si l'écriture est double, ne concerne pas la banque principale (1)
+					/* Si l'écriture est double, 
+					ne concerne pas la banque principale (1)
 					et dont la soeur n'a pas été encore traitée */
-					if (!is_null($ecriture->is_double) and $ecriture->banque_id != 1) {
-						if (!in_array($ecriture->id, $this->skip)) {
-						$this->solde[$ecriture->banque_id] +=  $ecriture->montant;
-						$this->solde[$ecriture->banque_soeur_id] -=  $ecriture->montant;
-						// $this->solde['total'] += $ecriture->montant;
+					if (!is_null($ecriture->is_double) 
+						and $ecriture->banque_id != 1) 
+					{
+						if (!in_array($ecriture->id, $this->skip)) 
+						{
 
-						// et on ajoute l'écriture soeur à la liste des skip
-						$this->skip[] = $ecriture->soeur_id;
+							/* Considérant un ordre de priorité en faveur des banques 
+							ayant l'id le plus faible, on compare les 2 id.
+							S'il est plus faible on traite cette écriture et on "skip" sa soeur.
+							Sinon on fait l'inverse */
+							if($ecriture->banque_id < $ecriture->banque_soeur_id)
+							{
+								$this->solde[$ecriture->banque_id] +=  $ecriture->montant;
+								$this->solde[$ecriture->banque_soeur_id] -=  $ecriture->montant;
+
+								// On ajoute l'écriture soeur à la liste des skip
+								$this->skip[] = $ecriture->soeur_id;
+							}else{
+
+								// On ne tient pas compte de cette écriture
+								unset($ecritures[$ecriture->rang]);
+							}
 						}else{
-						// On ne tient pas compte de cette écriture
+							// Si elle est dans ce tableau elle saute !
 							unset($ecritures[$ecriture->rang]);
 						}
-					}
 
+					}
 					/*  On affecte les soldes à l'écriture */
 					$ecriture->solde_1 = $this->solde[1];
 					$ecriture->solde_2 = $this->solde[2];
@@ -117,23 +133,16 @@ class PrevRepository {
 					$ecriture->solde_4 = $this->solde[4];
 					$ecriture->solde_total = $this->solde['total'];
 
-
-					/*  On affiche ou non chaque solde selon qu'il a changé ou non*/
+					/*  On affiche ou non chaque solde selon qu'il a changé ou non */
 					$ecriture->show_1 = ($this->solde[1] == $prev_solde_1)? false : true;
 					$ecriture->show_2 = ($this->solde[2] == $prev_solde_2)? false : true;
 					$ecriture->show_3 = ($this->solde[3] == $prev_solde_3)? false : true;
 					$ecriture->show_4 = ($this->solde[4] == $prev_solde_4)? false : true;
 				}
+			}
+		});
+	return $ecritures;
 
-			} //foreach banque
-
-		}); //each $ecriture
-
-				// dd($ecritures);
-
-// dd('fin');
-return $ecritures;
-
-}
+	}
 
 }
