7c5ff970cd4e72898c1744b356afb9cb7277b7b3
gAb09 <brunogabiot@club-internet.fr>
2014-10-26 18:37:37 +0100

    Réécriture titre page ecriture.edit

--- app/lib/frontend/tresorerie/controllers/EcritureController.php
+++ app/lib/frontend/tresorerie/controllers/EcritureController.php
@@ -209,11 +209,12 @@ class EcritureController extends BaseController {
 	public function edit($id)
 	{
 		$ec1 = Ecriture::where('id', $id)->with('ecriture2')->first();
+		$libelleDetail = ($ec1->libelle_detail)? ' - '.$ec1->libelle_detail : "";
 
 		return View::Make('frontend/tresorerie/views/ecritures/edit')
 		->with('ecriture', $ec1)
 		->with('list', self::getListes())
-		->with('titre_page', "Édition de l’écriture \"$ec1->libelle - $ec1->libelle_detail\" (n°$ec1->id)")
+		->with('titre_page', "Édition de l’écriture \"$ec1->libelle$libelleDetail\" (n°$ec1->id)")
 		;
 	}
 
